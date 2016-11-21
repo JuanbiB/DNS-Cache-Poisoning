396bf955955fc02d6baa445c7224fb948ff3849d
jamescapuder <capuderjames@gmail.com>
2016-11-20 20:37:28 -0500

    ok TXID check back :lock:
    ice ice baby

--- DNS.java
+++ DNS.java
@@ -4,7 +4,8 @@ public class DNS implements Node{
     //Instance Variables: Root name server and cache
     private NameServer root_;
     private Cache cache_;
-
+    private int TXID_;
+    private Node client_;
 
     /**
      * Constructor for DNS server object.
@@ -28,15 +29,23 @@ public class DNS implements Node{
         if (message.getType() == MessageTypes.FINAL){
             // Add entry to cache
             // add TXID check
-            cache_.addEntry(message.getQuery(), message.getAnswer());
-            src.message(this, message)
+            if (message.getTXID()==this.TXID_){
+                this.cache_.addEntry(message.getQuery(), message.getAnswer());
+                this.client_.message(this, message);
+            }
         }
         if (message.getType()==MessageTypes.WHERE){
+            this.TXID_ = message.getTXID();
+            this.client_ = src;
             root_.message(this, message)
         }
         if (message.getType()==MessageTypes.TRY){
-            NameServer nextServer = message.getNextServer();
-            nextServer.message(this, new Message(message.getQuery()));
+            if (message.getTXID()==this.TXID_){
+                NameServer nextServer = message.getNextServer();
+                nextServer.message(this, new Message(message.getQuery()));
+            }else{
+                return;
+            }
         }
     }
 }
