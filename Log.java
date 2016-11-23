9edb860901fb63699de250d76ff71f14178285ca
Eguendel <eguendel@oberlin.edu>
2016-11-22 21:17:39 -0500

    definitions will change, headers will stay the same

--- Log.java
+++ Log.java
@@ -1,16 +1,16 @@
 
 public class Log {
 
-	public static void w(String warning) {
+	public static void w(String tag, String warning) {
 		System.err.println(warning);
 	}
 	
-	public static void e(String error) {
+	public static void e(String tag, String error) {
 		System.err.println(error);
 		System.exit(1);
 	}
 	
-	public static void i(String info) {
+	public static void i(String tag, String info) {
 		System.out.println(info);
 	}
 }
